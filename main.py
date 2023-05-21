15d0bff0811cff9b7e93db9b08b5ea43145bea00
SyabAhmad <syabblogger@gmail.com>
2023-05-21 18:15:52 +0500

    Giving Error, trying to resolve

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
+from sklearn.preprocessing import LabelEncoder
+
 print("Email Spam Detection")
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-
+from sklearn.metrics import mean_squared_error
 data = pd.read_csv("dataset/emails.csv")
 # print(data.head(5))
 # print(data.shape)
 # print(data.info)
 # print(data.describe())
 print(data.columns)
+
 print(data["spam"])
 
 print(data.isna().sum())
+data = data.fillna("")
+print(data.isna().sum())
+
+LabelEncoding = LabelEncoder()
+
+cleanData = pd.concat([data["text"], data["spam"]], axis=1)
+#
+# for column in cleanData.columns:
+#     cleanData[column] = LabelEncoding.fit_transform(cleanData[column])
+
+
+cleanData["spam"] = LabelEncoding.fit_transform(cleanData["spam"])
+cleanData["text"] = LabelEncoding.fit_transform(cleanData["text"])
+
+print(cleanData["text"])
+
+print(cleanData.head(5))
+regressor = LinearRegression()
+
+
+
+xtrain, xtest, ytrain, ytest = train_test_split(cleanData["text"], data["spam"], test_size=20, random_state=40)
+
+xtrain = xtrain.values.reshape(-1, 1)
+ytrain = ytrain.values.reshape(-1, 1)
+xtest = xtest.values.reshape(-1, 1)
+
+regressor.fit(xtrain, ytrain)
+
+predction = regressor.predict(xtest)
+mse = mean_squared_error(ytest, predction)
+print("mean_squared_error: ", mse)
